82e959de6876a1788be68b54336241224a61cdd4
Najuf Malik <71990543+najufmalik@users.noreply.github.com>
2021-10-31 17:59:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 /* BIO BUTTON */
 function displayBio() {
   //let printBio = document.querySelector("#about");
-  if(document.getElementById("bio").clicked == true)
-  {
-    document.getElementById("about").style.display = "block";
-  }
+  document.getElementById("about").style.display = "block";
 }
