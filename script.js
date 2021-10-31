d890fdc26440da4f6af80bcf6e6c2c0e1b965081
Najuf Malik <71990543+najufmalik@users.noreply.github.com>
2021-10-31 15:27:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-/* Linkes dropdown */
+/* DROPDOWN LINKS */
 
-    $('.drop-links').hide();
-    $('#links-button').click(function(){
-        $('.drop-links').slideToggle(500);
-        $(this).toggleClass('drop-clicked');
-        return false;
-    });
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
