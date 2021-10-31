1c45e042b0b956c781a3b15d7b58cd14ebdaaea5
Najuf Malik <71990543+najufmalik@users.noreply.github.com>
2021-10-31 12:09:17 -0500

    Update script.js
    Added dropdown links

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-/* Bio Buttons */
+/* Linkes dropdown */
+
+    $('.drop-links').hide();
+    $('#links-button').click(function(){
+        $('.drop-links').slideToggle(500);
+        $(this).toggleClass('drop-clicked');
+        return false;
+    });
