237df3f5089240c9f31b4bb83085769b86d1cf15
Najuf <71990543+najufmalik@users.noreply.github.com>
2022-11-10 20:33:14 -0600

    Update script.js
    Starting over

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
 /* BIO BUTTON */
-var clicked = false;
-
-function displayBio() {
-  if(clicked)
-  {
-    document.getElementById("about").style.display = "none";
-  }
-  
-  else
-  {
-    document.getElementById("about").style.display = "block";
-  }
-  clicked = !clicked;
-}
 
